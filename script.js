936a300d03ae79b8b6497b364930682329faeac0
Muhammad Tayyab <136323260+mtayyab0012@users.noreply.github.com>
2026-06-06 03:39:50 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var _0x4a2b=["1-Greeting"," Thanks"," Great ","CanYou","2-Opening","CanISpeak","Busy","Comapny","Home","HowLong","RealPerson","CallYouBack","HvSys","Upgrade","Benefits","DogGun","IfWe","NI","Yes","Dog","QXfer","Afford","No","Dnc","IsThatOk","LetMe","POA","Good"];
+var _s={},_ca=null,_cs=null;
+(function(_l){_l.forEach(function(_n){var _a=new Audio("sounds/"+_n+".mp3");_a.preload="auto";_s[_n]=_a;})})(_0x4a2b);
+function playSound(_x){var _a=_s[_x];if(!_a)return;if(_ca&&_cs===_x&&!_ca.paused)return;if(_ca){_ca.pause();_ca.currentTime=0;}_ca=_a;_cs=_x;_ca.currentTime=0;_ca.play().catch(function(_e){});_ca.onended=function(){_ca=null;_cs=null;};}
+function stopSound(){if(_ca){_ca.pause();_ca.currentTime=0;_ca=null;_cs=null;}}
