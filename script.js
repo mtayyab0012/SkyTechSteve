129920df127077b9a271072a178d8d776afff907
Muhammad Tayyab <136323260+mtayyab0012@users.noreply.github.com>
2026-07-07 02:00:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var _0x4a2b=["1-Greeting","Name","cf","Greet2","opnew","Thanks2","Opening2","Hello","There","Hear","Good","Repeat","HoldOn","Great","CanYou","OpHome","CanISpeak","Busy","Company","Home","CanISay","RealPerson","CallYouBack","HvSys","Upgrade","Benefits","DogGun","IfWe","NI","YES","Dog","QXfer","Afford","NO","OK","IsThatOk1","Rescue","POA","Good"];
+var _0x4a2b=["1-Greeting","Name","HowMuch","cf","Greet2","opnew","Thanks2","Opening2","Hello","There","Hear","Good","Repeat","HoldOn","Great","CanYou","OpHome","CanISpeak","Busy","Company","Home","CanISay","RealPerson","CallYouBack","HvSys","Upgrade","Benefits","DogGun","IfWe","NI","YES","Dog","QXfer","Afford","NO","OK","IsThatOk1","Rescue","POA","Good"];
 var _s={},_ca=null,_cs=null;
 (function(_l){_l.forEach(function(_n){var _a=new Audio("sounds/"+_n+".mp3");_a.preload="auto";_s[_n]=_a;})})(_0x4a2b);
 function playSound(_x){var _a=_s[_x];if(!_a)return;if(_ca&&_cs===_x&&!_ca.paused)return;if(_ca){_ca.pause();_ca.currentTime=0;}_ca=_a;_cs=_x;_ca.currentTime=0;_ca.play().catch(function(_e){});_ca.onended=function(){_ca=null;_cs=null;};}
